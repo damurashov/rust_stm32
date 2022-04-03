5bbe20de5845d811533d5a220210788bf32260f5
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-03 16:08:07 +0300

    [:build] Int | asm code, hard_fault_trampoline

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+use cc;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
     File::create(out_dir.join("script.ld"))?.write_all(include_bytes!("script.ld"))?;
+
+    // Assembly code
+    cc::Build::new().file("init.s").compile("asm");
+    println!("cargo:rerun-if-changed=init.s");
 
     Ok(())
 }
