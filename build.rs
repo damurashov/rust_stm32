eccfaf92afe954ec610194504d0a5043e3daa1c1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-24 16:08:09 +0300

    [:build] Int | Linked against static `libc` and `libnosys`

--- build.rs
+++ build.rs
@@ -4,7 +4,10 @@ use cc;
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-search={}/lib/", env::current_dir().unwrap().to_str().unwrap());
     File::create(out_dir.join("script.ld"))?.write_all(include_bytes!("script.ld"))?;
+    println!("cargo:rustc-link-lib=nosys");
+    println!("cargo:rustc-link-lib=c_nano");
 
     // Assembly code
     cc::Build::new().file("init.s").compile("asm");
