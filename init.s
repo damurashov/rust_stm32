475d60f879ec692a0a0893a2b455200a131dca75
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-10 11:32:28 +0300

    [:init.s] Impl | Reset trampoline: stack switch
    Switch to PSP stack on startup

--- init.s
+++ init.s
@@ -4,3 +4,19 @@
 hard_fault_trampoline:
     mrs r0, MSP /* Load the 1st arg - stack pointer value */
     b hard_fault /* Proceed w/ the user implementation of hardfault handler, if there's one. See "script.ld" and "lib.rs" */
+
+.section .rodata
+.word EXC_RETURN
+
+    .section .text.reset_trampoline
+    .global reset_trampoline
+    .type reset_trampoline, %function
+reset_trampoline:
+    /* Copy MSP to PSP */
+    mrs r0, MSP
+    msr PSP, r0
+    /* Use PSP stack pointer instead of default MSP */
+    movs R0, #0x2
+    msr CONTROL, r0
+    isb  /* Apply instruction barrier to force use of the new stack pointer */
+    b reset
