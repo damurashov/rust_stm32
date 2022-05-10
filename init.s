03735189a262b9fbc7f14d89fc3f876285b81945
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-10 16:42:44 +0300

    INTERMEDIATE

--- init.s
+++ init.s
@@ -5,18 +5,19 @@ hard_fault_trampoline:
 	mrs r0, MSP /* Load the 1st arg - stack pointer value */
 	b hard_fault /* Proceed w/ the user implementation of hardfault handler, if there's one. See "script.ld" and "lib.rs" */
 
-.section .rodata
-.word EXC_RETURN
+.word _psp_initial
+val_psp_initial: .word _psp_initial
 
 	.section .text.reset_trampoline
 	.global reset_trampoline
 	.type reset_trampoline, %function
 reset_trampoline:
-	/* Copy MSP to PSP */
-	mrs r0, MSP
+	/* Set initial value for PSP */
+	ldr r0, =val_psp_initial
+	ldr r0, [r0]
 	msr PSP, r0
 	/* Use PSP stack pointer instead of default MSP */
 	movs R0, #0x2
 	msr CONTROL, r0
-	isb  /* Apply instruction barrier to force use of the new stack pointer */
+	isb  /* Apply instruction barrier */
 	b reset
